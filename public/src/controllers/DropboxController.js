36e83e06e716663378a21968a5f978760d9ab00f
Douglas <douglasvitor.dev@gmail.com>
2022-06-24 10:24:38 -0300

    Section 6.4: Upload files in clientside

--- public/src/controllers/DropboxController.js
+++ public/src/controllers/DropboxController.js
@@ -12,8 +12,38 @@ class DropboxController {
             this.inputFilesEl.click();
         });
 
-        this.inputFilesEl.addEventListener('change', () => {
+        this.inputFilesEl.addEventListener('change', event => {
+            this.uploadFiles(event.target.files)
             this.progressBarEl.style.display = 'block';
         })
+    }
+
+    uploadFiles(files) {
+        let promises = [];
+
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+                let request = new XMLHttpRequest();
+                request.open('POST', '/upload');
+                request.onload = () => {
+                    try {
+                        resolve(JSON.parse(request.responseText));
+                    } catch (error) {
+                        reject(error);                        
+                    }
+                }
+
+                request.onerror = () => {
+                    reject(error);
+                }
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+
+                request.send(formData);
+            }));
+        });
+
+        return Promise.all(promises);
     }
 }
